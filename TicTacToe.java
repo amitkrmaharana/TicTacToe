4d96b05b1ffb9f0e4301a0f66e0ba9e6423087bb
amitkrmaharana <ak657harley@gmail.com>
2021-02-13 09:41:20 +0530

    Added: created board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,13 @@
 public class TicTacToe {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome To tictactoe game");
+	public void gameBoard() {
+		char[] board = new char[10];
+		System.out.println(board);
 	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to tictactoe game");
+		TicTacToe game = new TicTacToe();
+		game.gameBoard();
+	}
+
 }
+
